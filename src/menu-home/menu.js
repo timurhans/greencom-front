f1b7e6c5909dc150776143ec7794537248ee15a9
LuishgFarias <luishgfarias@gmail.com>
2022-11-17 15:14:59 -0300

    menu complete

--- src/menu-home/menu.js
+++ src/menu-home/menu.js
@@ -1,109 +1,164 @@
-import React, { useState,useRef }  from 'react';
-import { Menubar } from 'primereact/menubar';
-import { Menu } from 'primereact/menu';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { useLocalStorage } from '../requests/greenHooks.js'
-
+import React, { useState, useRef, useEffect } from "react";
+import "./menu.css";
+import { Menubar } from "primereact/menubar";
+import { Menu } from "primereact/menu";
+import { Button } from "primereact/button";
+import { SlideMenu } from "primereact/slidemenu";
+import { InputText } from "primereact/inputtext";
+import { useLocalStorage } from "../requests/greenHooks.js";
+import { Sidebar } from "primereact/sidebar";
+import { Image } from "react-bootstrap";
 //Menu com dropdown para pedidos,clientes e etc
 const MenuPerfil = () => {
-    const menu = useRef(null);
-    const [username, ] = useLocalStorage("username",null)
-    const [isRep, ] = useLocalStorage("isRep",null)
-    
-    let items_perfil = [
-            {
-                label: 'Conta',
-                icon: 'pi pi-user',
-                url: '/conta'
-            },
-            {
-                label: 'Pedidos',
-                icon: 'pi pi-book',
-                url: '/pedidos'
-            },
+  const menu = useRef(null);
+  const [username] = useLocalStorage("username", null);
+  const [isRep] = useLocalStorage("isRep", null);
 
-        ]
-        if (isRep){
-            items_perfil.push({
-                label: 'Clientes',
-                icon: 'pi pi-users',
-                url: '/clientes'
-            })
-            items_perfil.push({
-                label: 'Promocoes',
-                icon: 'pi pi-percentage',
-                url: '/promocoes'
-            })
-            items_perfil.push({
-                label: 'Trade',
-                icon: 'pi pi-flag',
-                url: '/trade'
-            })
+  let items_perfil = [
+    {
+      label: "Conta",
+      icon: "pi pi-user",
+      url: "/conta",
+    },
+    {
+      label: "Pedidos",
+      icon: "pi pi-book",
+      url: "/pedidos",
+    },
+  ];
+  if (isRep) {
+    items_perfil.push({
+      label: "Clientes",
+      icon: "pi pi-users",
+      url: "/clientes",
+    });
+    items_perfil.push({
+      label: "Promocoes",
+      icon: "pi pi-percentage",
+      url: "/promocoes",
+    });
+    items_perfil.push({
+      label: "Trade",
+      icon: "pi pi-flag",
+      url: "/trade",
+    });
+  }
+  const items = [
+    {
+      label: username,
+      items: items_perfil,
+    },
+  ];
 
-        }    
-    const items = [
-        {   
-            label:username,
-            items: items_perfil
-        }
-    ]
+  return (
+    <>
+      <Menu model={items} popup ref={menu} id="popup_menu" />
+      <Button
+        icon="pi pi-bars"
+        onClick={(event) => menu.current.toggle(event)}
+        aria-controls="popup_menu"
+        aria-haspopup
+      />
+    </>
+  );
+};
 
+export function MyMenu(props) {
+  const [itemsDesk] = useLocalStorage("categorias",[])
+  const [itemMob,] = useLocalStorage("categoriasMobile",[])
+  const [visibleRight, setVisibleRight] = useState(false);
+  const [query, setQuery] = useState("");
+  const [tipoConta] = useLocalStorage("tipoConta", []);
 
-
-    return (
-        <>
-            <Menu model={items} popup ref={menu} id="popup_menu" />
-            <Button icon="pi pi-bars" onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup />
-        </>
-    );
-}
-
-
-export function MyMenu(props){
-    const [items,] = useLocalStorage("categorias",[])
-    const [query,setQuery] = useState('')
-    const [tipoConta,] = useLocalStorage("tipoConta",[])
-
-    const handleSearch = (e) =>{
-        if (query !== ''){
-            e.preventDefault()
-            window.location = '/busca?query='+query
-            setQuery('')
-        }
-
+  const handleSearch = (e) => {
+    if (query !== "") {
+      e.preventDefault();
+      window.location = "/busca?query=" + query;
+      setQuery("");
     }
-    const handleChangeSearch = (e) =>{
-        //Faz submit automaticamente na busca caso seja codigo de barras
-        setQuery(e.target.value)
-        if (e.target.value.length >= 13 && /^\d+$/.test(e.target.value)){
-            e.preventDefault()
-            window.location = '/busca?query='+e.target.value
-            setQuery('')
-        }
-
+  };
+  const handleChangeSearch = (e) => {
+    //Faz submit automaticamente na busca caso seja codigo de barras
+    setQuery(e.target.value);
+    if (e.target.value.length >= 13 && /^\d+$/.test(e.target.value)) {
+      e.preventDefault();
+      window.location = "/busca?query=" + e.target.value;
+      setQuery("");
     }
+  };
 
-
-    const start = <a href="/"><img alt="logo" src="https://ondasstr092020.blob.core.windows.net/modelo/logo.png" height="40" className="p-mr-2"></img></a>
-    //Mostra lateral do menu apenas para usuario logado
-    let end = <a href="/login">Login</a>
-    if(props.loggedIn){
-        
-        end = <> 
-        <InputText ref={props.searchInput} onChange={handleChangeSearch} placeholder="Buscar" type="text" />
-        <Button onClick={handleSearch} icon="pi pi-search" className="p-button-rounded p-button-secondary" />
-        <Button icon="pi pi-shopping-cart" onClick={(e) => window.location.href="/carrinho"} aria-controls="popup_menu" aria-haspopup />
-        {tipoConta==="visitante" ? <Button icon="pi pi-user" onClick={(e) => window.location.href="/conta"} aria-controls="popup_menu" aria-haspopup /> : <MenuPerfil></MenuPerfil> }
-        </>
-    }
-
-    return (
-        <div>
-            <div className="card">
-                <Menubar model={items} start={start} end={end} />
-            </div>
-
+  const start = (
+    <a href="/">
+      <img
+        alt="logo"
+        src="https://ondasstr092020.blob.core.windows.net/modelo/logo.png"
+        className="p-mr-2"
+      />
+    </a>
+  );
+  //Mostra lateral do menu apenas para usuario logado
+  const button = (
+    <div className="btnMenu" onClick={() => setVisibleRight(true)}>
+      <Image src="menu.svg"/>
+    </div>
+  );
+  let end = <a href="/login">Login</a>;
+  if (props.loggedIn) {
+    end = (
+      <div className="endMenu">
+        <div className="search">
+          <InputText
+            ref={props.searchInput}
+            onChange={handleChangeSearch}
+            placeholder="Buscar"
+            type="text"
+          />
+          <Button
+            onClick={handleSearch}
+            icon="pi pi-search"
+            className="p-button-rounded p-button-secondary"
+          />
         </div>
-    )
+        <div className="btns">
+          <Button
+            icon="pi pi-shopping-cart"
+            onClick={(e) => (window.location.href = "/carrinho")}
+            aria-controls="popup_menu"
+            aria-haspopup
+          />
+          {tipoConta === "visitante" ? (
+            <Button
+              icon="pi pi-user"
+              onClick={(e) => (window.location.href = "/conta")}
+              aria-controls="popup_menu"
+              aria-haspopup
+            />
+          ) : (
+            <MenuPerfil />
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="card menu">
+        <Menubar model={itemsDesk} start={start} end={end} className="menu" />
+      </div>
+      <div className="card menuMobile">
+        <header className="menuMobile">
+          {start} {button}
+        </header>
+      </div>
+      <Sidebar
+        visible={visibleRight}
+        position="right"
+        onHide={() => setVisibleRight(false)}
+      >
+        <p>{end}</p>
+        <SlideMenu model={itemMob} />
+      </Sidebar>
+    </>
+  );
 }
--- src/produtos/produtos.js
+++ src/produtos/produtos.js
@@ -1,114 +1,142 @@
 import React from "react";
-import { useEffect, useState } from 'react'
-import { useProductSearch,useLocalStorage } from '../requests/greenHooks.js'
-import CardProduto from './cardProduto.js'
-import 'primeflex/primeflex.css';
-import { Paginator } from 'primereact/paginator';
-import { Dialog } from 'primereact/dialog';
-import { Button } from 'primereact/button';
-import { Dropdown } from 'primereact/dropdown';
-
+import { useEffect, useState } from "react";
+import { useProductSearch, useLocalStorage } from "../requests/greenHooks.js";
+import CardProduto from "./cardProduto.js";
+import "primeflex/primeflex.css";
+import { Paginator } from "primereact/paginator";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { Dropdown } from "primereact/dropdown";
 
 export default function Produtos(props) {
-
-  const [listItems,setListItems] = useState(<div></div>)  
-  const [first, setFirst] = useState(0)
-  const [rows, setRows] = useState(12)
-  const [qtd, setQtd] = useState(0)
-  const [token,setToken] = useLocalStorage("token",null)
-  const [colecao, setColecao] = useLocalStorage("colecao","Todas")
-  const [periodo, setPeriodo] = useLocalStorage("periodo","Todos")
-  const [orderBy, setOrderBy] = useLocalStorage("orderBy","produto")
-  const [clienteId, ] = useLocalStorage("clienteId",null)
-  const [tamColuna,setTamColuna] = useLocalStorage("tamColuna",2)
-  const [classColuna,setclassColuna] = useState("p-col-12 p-md-6 p-lg-"+tamColuna)
+  const [listItems, setListItems] = useState(<div></div>);
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(12);
+  const [qtd, setQtd] = useState(0);
+  const [token, setToken] = useLocalStorage("token", null);
+  const [colecao, setColecao] = useLocalStorage("colecao", "Todas");
+  const [periodo, setPeriodo] = useLocalStorage("periodo", "Todos");
+  const [orderBy, setOrderBy] = useLocalStorage("orderBy", "produto");
+  const [clienteId] = useLocalStorage("clienteId", null);
+  const [tamColuna, setTamColuna] = useLocalStorage("tamColuna", 2);
+  const [classColuna, setclassColuna] = useState(
+    "p-col-12 p-md-6 p-lg-" + tamColuna
+  );
   const [displayModal, setDisplayModal] = useState(false);
-  const [colecoesOptions,] = useLocalStorage("colecoes",[])
-  const [periodosOptions,] = useLocalStorage("periodos",[])
+  const [colecoesOptions] = useLocalStorage("colecoes", []);
+  const [periodosOptions] = useLocalStorage("periodos", []);
 
-
-  const tamColOptions = [2,3,4,6,12]
-  const orderByOptions = ["produto","estoque","desconto"]
+  const tamColOptions = [2, 3, 4, 6, 12];
+  const orderByOptions = ["produto", "estoque", "desconto"];
 
   const onClick = () => {
-    setDisplayModal(true)
-  }
+    setDisplayModal(true);
+  };
   const onHide = () => {
-      setDisplayModal(false)
-  }
+    setDisplayModal(false);
+  };
 
   useEffect(() => {
-    document.title = "Greenish B2B | Produtos"
-  }, [])
+    document.title = "Greenish B2B | Produtos";
+  }, []);
 
   //-------------------
 
-  const {
-    produtos,isBarCode
-  } = useProductSearch(colecao,periodo,clienteId,orderBy)
+  const { produtos, isBarCode } = useProductSearch(
+    colecao,
+    periodo,
+    clienteId,
+    orderBy
+  );
 
-  console.log(isBarCode)
-
+  console.log(isBarCode);
 
   const handlePageChange = (event) => {
     setFirst(event.first);
     setRows(event.rows);
-  }
-  
+  };
+
   useEffect(() => {
-    if ('lista' in produtos){
-      setQtd(produtos['lista'].length)
-      const lista = produtos['lista'].slice(first,first+rows)
-      let listItemsProv = <div className={classColuna}>Entrou</div>
-      listItemsProv = lista.map((val,index) => <div className={classColuna}><CardProduto
-       produto={val} isBarCode={isBarCode} key={index} searchInput={props.searchInput}></CardProduto></div>)
-      setListItems(listItemsProv)
-      window.scrollTo(0, 0)
+    if ("lista" in produtos) {
+      setQtd(produtos["lista"].length);
+      const lista = produtos["lista"].slice(first, first + rows);
+      let listItemsProv = <div className={classColuna}>Entrou</div>;
+      listItemsProv = lista.map((val, index) => (
+        <div className={classColuna}>
+          <CardProduto
+            produto={val}
+            isBarCode={isBarCode}
+            key={index}
+            searchInput={props.searchInput}
+          ></CardProduto>
+        </div>
+      ));
+      setListItems(listItemsProv);
+      window.scrollTo(0, 0);
     }
-  }, [produtos,classColuna,first,rows,isBarCode])
+  }, [produtos, classColuna, first, rows, isBarCode]);
 
   useEffect(() => {
-    setclassColuna("p-col-12 p-md-"+tamColuna+" p-lg-"+tamColuna)
-  }, [tamColuna])
-
+    setclassColuna("p-col-12 p-md-" + tamColuna + " p-lg-" + tamColuna);
+  }, [tamColuna]);
 
   return (
     <>
-      <div>
-        
-        <Button label="Filtrar" onClick={() => onClick()} className="p-mr-2" />
-            <Dialog header={"Filtros"} visible={displayModal} style={{ width: '75vw' }} onHide={() => onHide()}>
-                <div className="p-grid">
-                    <div className="p-col-12 p-md-6 p-lg-2">
-                        <h3>Colecao</h3>
-                        <Dropdown id="dropdown" value={colecao} options={colecoesOptions}
-                        onChange={(e) => setColecao(e.value)}
-                        />
-                    </div>
-                    <div className="p-col-12 p-md-6 p-lg-2">
-                        <h3>Periodo</h3>
-                        <Dropdown id="dropdown" value={periodo} options={periodosOptions}
-                        onChange={(e) => setPeriodo(e.value)}
-                        />
-                    </div>
-                    <div className="p-col-12 p-md-6 p-lg-2">
-                        <h3>Ordenar por</h3>
-                        <Dropdown id="dropdown" value={orderBy} options={orderByOptions}
-                        onChange={(e) => setOrderBy(e.value)}
-                        />
-                    </div>                  
-                    <div className="p-col-12 p-md-6 p-lg-2">
-                        <h3>Tamanho Imagem</h3>
-                        <Dropdown id="dropdown" value={tamColuna} options={tamColOptions}
-                        onChange={(e) => setTamColuna(e.value)}
-                        />
-                    </div>                                             
-                </div>
-            </Dialog>
-      </ div>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-around",
+            gap: '1rem',
+            fontSize: 'smaller',
+            padding: '.5em'
+          }}
+        >
+            <div>
+              <h3>Colecao</h3>
+              <Dropdown
+                id="dropdown"
+                value={colecao}
+                options={colecoesOptions}
+                onChange={(e) => setColecao(e.value)}
+              />
+            </div>
+            <div>
+              <h3>Periodo</h3>
+              <Dropdown
+                id="dropdown"
+                value={periodo}
+                options={periodosOptions}
+                onChange={(e) => setPeriodo(e.value)}
+              />
+            </div>
+            <div>
+              <h3>Ordenar por</h3>
+              <Dropdown
+                id="dropdown"
+                value={orderBy}
+                options={orderByOptions}
+                onChange={(e) => setOrderBy(e.value)}
+              />
+            </div>
+            <div>
+              <h3>Tamanho</h3>
+              <Dropdown
+                id="dropdown"
+                value={tamColuna}
+                options={tamColOptions}
+                onChange={(e) => setTamColuna(e.value)}
+              />
+          </div>
+      </div>
       <div className="p-grid">{listItems}</div>
-      <Paginator first={first} rows={rows} totalRecords={qtd} rowsPerPageOptions={[12, 24, 36]} onPageChange={handlePageChange}></Paginator>
+      <Paginator
+        first={first}
+        rows={rows}
+        totalRecords={qtd}
+        rowsPerPageOptions={[12, 24, 36]}
+        onPageChange={handlePageChange}
+      ></Paginator>
     </>
-  )
+  );
 }
-
